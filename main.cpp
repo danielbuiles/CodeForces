5c60b4554a994f54cdbdc03be3173f3da7e2770f
CmdData <61318415+danielbuiles@users.noreply.github.com>
2022-01-09 18:39:56 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<iomanip>
+#include<math.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+int main(){
+  string palabra,last,first;
+  int ciclo,i=0;
+  cin>>ciclo;
+  while(i<ciclo){
+    cin>>palabra;
+    if(palabra.size()>10){
+      for(int i=0;i<palabra.size();i++){
+        if(i==0){
+          first=palabra[i];
+        }else if(i==palabra.size()-1){
+          last=palabra[i];
+        }
+      }
+      cout<<first<<to_string(palabra.size()-2)<<last<<endl;
+    }else{
+      cout<<palabra<<endl;
+    }
+    i++;
+  }
+  return 0;
+}
